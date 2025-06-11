2cf75448db362eceb2804f6338ee262b28219756
Josh Mu <hello@joshmu.dev>
2025-06-11 18:18:53 +1000

    feat(current-file): support for extension command to automatically scope search to current file
    Resolves #84

--- src/lib/periscope.ts
+++ src/lib/periscope.ts
@@ -2,9 +2,14 @@ import { context as cx } from './context';
 import { onDidHide, setupQuickPickForQuery, setupRgMenuActions } from './quickpickActions';
 import { start } from './globalActions';
 import { openInHorizontalSplit } from './editorActions';
+import { setCurrentFileContext } from '../utils/searchCurrentFile';
 
-function search() {
+function search({ currentFileOnly = false }: { currentFileOnly?: boolean } = {}) {
   start();
+
+  if (currentFileOnly) {
+    setCurrentFileContext();
+  }
 
   // if ripgrep actions are available then open preliminary quickpick
   const showRgMenuActions = cx.config.alwaysShowRgMenuActions && cx.config.rgMenuActions.length > 0;
